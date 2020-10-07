0cf4240936bfb48a717ce9707204266a5e17258e
DeefChuanbiaoLi <cb17658113837@163.com>
2020-10-07 11:20:49 +0800

    xvavx

--- GitStudy/src/Main/MainTask.java
+++ GitStudy/src/Main/MainTask.java
@@ -3,6 +3,8 @@
  */
 package Main;
 
+import java.util.ArrayList;
+
 /**
  * @author ASUS-PC
  *
@@ -14,7 +16,15 @@ public class MainTask {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			System.out.println("fsaufbea");
+		ArrayList arr = new ArrayList();
+		arr.add(50);
+		arr.add(60);
+		arr.add(40);
+		arr.add(90);
+		arr.add(30);
+		sort(arr);
 	}
-
+	public static void sort(ArrayList arr) {
+		
+	}
 }
--- StudyJava/src/Main.java
+++ StudyJava/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 /**
  * 
  */
@@ -13,7 +15,18 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			System.out.println("ddvevc");
+			ArrayList arr = new ArrayList();
+			arr.add(50);
+			arr.add(60);
+			arr.add(40);
+			arr.add(90);
+			arr.add(30);
+			sort(arr);
+			
 	}
+	public static void sort(ArrayList arr) {
+		
+	}
+	
 
 }
